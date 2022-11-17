057b866ca8aac6d62f6f4cb6d775194f3ea4575c
mawa <mawahama05@gmail.com>
2022-11-16 20:35:50 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const moveDodgerLeft=()=>{
+    const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+
+const moveDodgerRight=()=>{
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
